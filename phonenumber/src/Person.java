f70fe12ad5de713a74c026cf6aa84aef9740d3b1
연희 <@192.168.1.189>
2017-09-22 19:39:15 +0900

    생일 입력과 미입력을 구분할 수 있게 조건문을 넣었다.

--- phonenumber/src/Person.java
+++ phonenumber/src/Person.java
@@ -4,6 +4,8 @@ public class Person {
 	public static void main(String[] args) {
 		PhoneInfo per1 = new PhoneInfo("√÷ø¨»Ò", "01039587509", "971006");
 		PhoneInfo per2 = new PhoneInfo("±ËµøπŒ", "01027703108");
+		per1.printperson();
+		per2.printperson();
 	}
 
 }
--- phonenumber/src/PhoneInfo.java
+++ phonenumber/src/PhoneInfo.java
@@ -2,13 +2,25 @@ import java.util.*;
 
 
 public class PhoneInfo {
-	PhoneInfo(String name, String phonenum, String birthday) {
-		System.out.println("성함 " + name);
-		System.out.println("전화번호 " + phonenum);
-		System.out.println("생일 " + birthday);
-	}
-	PhoneInfo(String name, String phonenum) {
-		System.out.println("성함 " + name);
-		System.out.println("전화번호 " + phonenum);
-	}
+		String name;
+		String phonenum;
+		String birthday;
+		PhoneInfo(String name, String phonenum, String birthday) {
+			this.name = name;
+			this.phonenum = phonenum;
+			this.birthday = birthday;
+		}
+		PhoneInfo(String name, String phonenum){
+			this.name = name;
+			this.phonenum = phonenum;
+		}
+		public void printperson() {
+			System.out.println("성함 " + name);
+			System.out.println("전화번호 " + phonenum);
+			if(birthday==null)
+			{
+			System.out.println("생일 미입력");
+			}
+			else System.out.println("생일 " + birthday);
+		}
 }
