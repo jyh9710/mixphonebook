9ee6651bd801a3ab1a00c3146b564f4259e10a13
연희 <@192.168.0.15>
2017-09-30 23:46:30 +0900

    Main 프로그램 작성

--- phonenumber/src/PhoneInfo.java
+++ phonenumber/src/PhoneInfo.java
@@ -1,26 +1,29 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class PhoneInfo {
-		String name;
-		String phonenum;
-		String birthday;
-		PhoneInfo(String name, String phonenum, String birthday) {
-			this.name = name;
-			this.phonenum = phonenum;
-			this.birthday = birthday;
-		}
-		PhoneInfo(String name, String phonenum){
-			this.name = name;
-			this.phonenum = phonenum;
-		}
-		public void printperson() {
-			System.out.println("성함 " + name);
-			System.out.println("전화번호 " + phonenum);
-			if(birthday==null)
-			{
+	String name = null;
+	String phoneNumber = null;
+	String birthday = null;
+
+	PhoneInfo(String name, String phonenum, String birthday) {
+		this.name = name;
+		this.phoneNumber = phonenum;
+		this.birthday = birthday;
+	}
+
+	PhoneInfo(String name, String phonenum) {
+		this.name = name;
+		this.phoneNumber = phonenum;
+	}
+
+	public void showInfo() {
+		System.out.println("성함 " + name);
+		System.out.println("전화번호 " + phoneNumber);
+		if (birthday == null) {
 			System.out.println("생일 미입력");
-			}
-			else System.out.println("생일 " + birthday);
+		} else {
+			System.out.println("생일 " + birthday);
 		}
+	}
+
 }
